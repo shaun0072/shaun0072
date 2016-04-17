73712ff89b358219f5094553bf07774ad73621a0
Shaun Russell <shaundavidrussell@yahoo.com>
2016-04-17 10:55:46 -0500

    add smooth scroll on back to top

--- js/back-to-top.js
+++ js/back-to-top.js
@@ -1,11 +1,11 @@
 /* Link Scrolling */
 $('a[href^="#"]').on('click',function (e) {
-  e.preventDefault();
-​
-  var target = this.hash;
-  var $target = $(target);
-​
-    $('html, body').stop().animate({
+	e.preventDefault();  
+	var target = this.hash;
+	var $target = $(target);
+
+	$('html, body').stop().animate({
          'scrollTop': $target.offset().top
     }, 800, 'swing');
 });
+
